cb88958e7e9c61951f7c92ac35d7059c8d9feb4c
Alan Ramirez <aramireznicola@cnm.edu>
2021-10-11 09:18:44 -0600

    added arrow function

---
+++ functions-2/arrow-functions.js
@@ -0,0 +1,16 @@
+//declaring a function
+function myFunction (argument) {
+    console.log(argument)
+}
+//this function in a variable
+myOtherFunction = function(argument) {
+    console.log(argument)
+}
+//gives shorthand
+myArrowFunction = (argument) => {
+    console.log(argument)
+}
+
+myShorterArrowFunction = argument => console.log(argument)
+
+
--- functions-2/functions-2.js
+++ functions-2/functions-2.js
@@ -19,8 +19,5 @@ function convertWeightToGrams(weight, units) {
 console.log(convertWeightToGrams(2, 'kj'))
 
 
-
-
-
-
 //     Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number.  (The factors of a number are all numbers that divide evenly into it.  For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
+
