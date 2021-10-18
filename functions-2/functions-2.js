aefc9a84dc982252bb5c9eeb6014300929909e40
Alan Ramirez <aramireznicola@cnm.edu>
2021-10-18 08:37:35 -0600

    added navbar and dog cat picture

--- functions-2/functions-2.js
+++ functions-2/functions-2.js
@@ -21,3 +21,17 @@ console.log(convertWeightToGrams(2, 'kj'))
 
 //     Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number.  (The factors of a number are all numbers that divide evenly into it.  For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
 
+sumOfFactors = (number) => {
+    let sum = 0
+    for (let i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            sum = sum + i
+        }
+    }
+    return sum
+}
+
+console.log(sumOfFactors(13))
+
+
+
