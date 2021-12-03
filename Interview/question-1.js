097daf56c83bbb23e0b081df236005be5110c2ed
Alan Ramirez <aramireznicola@cnm.edu>
2021-12-03 09:49:04 -0700

    fizzbuzz 2

--- Interview/question-1.js
+++ Interview/question-1.js
@@ -18,3 +18,18 @@ function fizzBuzz (number1, number2) {
 }
 
 fizzBuzz(5, 30)
+
+function countNumbers (arrayOfNumbers) {
+    let counterArray = []
+    arrayOfNumbers.forEach(numberFromArray => counterArray[numberFromArray] = counterArray[numberFromArray] ? counterArray[numberFromArray] + 1 : 1)
+    return counterArray
+}
+
+console.log(countNumbers([3,2,3,5,4,3,5]))
+
+for (var i=1; i < 101; i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
